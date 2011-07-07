a36503fc7d7930df972fa288dee289e011d82046
uk <usk3@njit.edu>
2011-07-07 18:51:43 +0000

    Initial schema code

---
+++ MySchema/Thing.php
@@ -0,0 +1,171 @@
+<?php
+
+class Thing {
+
+	private $name;
+	private $url;
+	private $image;
+	private $description;
+	private $itemtype = '"http://www.schema.org/Thing"';
+
+	function getItemTypeValue() {
+
+		return $this->itemtype;
+
+	}
+	function getItemTypeAttributes() {
+	  
+		
+		$this->itemtype->attributes['itemprop'] = 'itemtype';
+
+	}
+
+	function setNameValue($var) {
+
+		$this->name->value = $var;
+
+	}
+
+	function getNameValue() {
+	
+		return $this->name->value;
+		
+	}
+	
+	function setNameTag($var) {
+	
+		$this->name->tag = $var;
+		
+	}
+
+	function getNameTag() {
+	
+		return $this->name->tag;
+		
+	}
+	function setNameAttributes($var) {
+	    
+	    //$class = get_class($this) . ' name ' . $var;
+		//$this->name->attributes['class'] = $class;
+		$this->name->attributes['itemprop'] = 'name';
+		
+	}
+
+	function getNameAttributes() {
+	
+		return $this->name->attributes;
+		
+	}
+
+	function setUrlValue($var) {
+	
+		$this->url->value = $var;
+		
+	}
+
+	function getUrlValue() {
+	
+		return $this->url->value;
+		
+	}
+	
+	function setUrlTag($var) {
+	
+		$this->url->tag = $var;
+		
+	}
+
+	function getUrlTag() {
+	
+		return $this->url->tag;
+		
+	}
+	function setUrlAttributes($var) {
+	    
+	    $class = get_class($this) . ' url ' . $var;
+		$this->url->attributes['class'] = $class;
+		$this->url->attributes['itemprop'] = 'url';
+		
+	}
+
+	function getUrlAttributes() {
+	
+		return $this->url->attributes;
+		
+	}
+	function setImageValue($var) {
+	
+		$this->image->value = $var;
+		
+	}
+
+	function getImageValue() {
+	
+		return $this->image->value;
+		
+	}
+	
+	function setImageTag($var) {
+	
+		$this->image->tag = $var;
+		
+	}
+
+	function getImageTag() {
+	
+		return $this->image->tag;
+		
+	}
+	function setImageAttributes($var) {
+	    
+	    $class = get_class($this) . ' image ' . $var;
+		$this->image->attributes['class'] = $class;
+		$this->image->attributes['itemprop'] = 'image';
+		
+	}
+
+	function getImageAttributes() {
+	
+		return $this->image->attributes;
+		
+	}
+	function setDescriptionValue($var) {
+	
+		$this->description->value = $var;
+		
+	}
+
+	function getDescriptionValue() {
+	
+		return $this->description->value;
+		
+	}
+	
+	function setDescriptionTag($var) {
+	
+		$this->description->tag = $var;
+		
+	}
+
+	function getDescriptionTag() {
+	
+		return $this->description->tag;
+		
+	}
+	function setDescriptionAttributes($var) {
+	    
+	    $class = get_class($this) . ' description ' . $var;
+		$this->description->attributes['class'] = $class;
+		$this->description->attributes['itemprop'] = 'description';
+		
+	}
+
+	function getDescriptionAttributes() {
+	
+		return $this->image->attributes;
+		
+	}
+
+}
+
+?>
